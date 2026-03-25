4974ea7ddd16434efef85479b58bb33b2947e7ce
Sruthi-Haiva <sruthi@haiva.ai>
2026-03-25 11:56:44 +0530

    fix vercel dist path

--- UI_Logs/vite.config.js
+++ UI_Logs/vite.config.js
@@ -1,7 +1,14 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
     }
   }
-}
+})
